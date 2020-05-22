025e13ba1fb3b962eeed84136463bb0e1efd1800
Dhruv Patel <starb9979@gmail.com>
2020-05-22 17:27:03 -0400

    removed unnecessary files

--- connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $connection = new PDO("mysql:host=localhost;dbname=baps", "root", "");
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
-?>
